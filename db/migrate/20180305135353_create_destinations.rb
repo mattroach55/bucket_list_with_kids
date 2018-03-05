c7cbb497fa8cf111ef248781be7b1de532d608f4
Matt Roach <mattroach@mac.com>
2018-03-05 17:06:25 +0100

    reset of DB models

--- db/migrate/20180305135353_create_destinations.rb
+++ db/migrate/20180305135353_create_destinations.rb
@@ -3,10 +3,23 @@ class CreateDestinations < ActiveRecord::Migration[5.1]
     create_table :destinations do |t|
       t.string :name
       t.string :description
+      t.string :street_number
       t.string :address
+      t.string :locality
+      t.string :country
+      t.string :region
       t.float :latitude
       t.float :longitude
+      t.string :photo
       t.string :type
+      t.string :holiday_type
+      t.string :theme
+      t.integer :kids_age_from
+      t.integer :kids_age_to
+      t.integer :duration
+      t.float :price
+      t.integer :bucket_list_count
+      t.integer :average_review_score
 
       t.timestamps
     end
