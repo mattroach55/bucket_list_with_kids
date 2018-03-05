6bdaaec3f8a9f52d00c3dfb53bb2e955678a5806
Julie VanderLaan julieannevl@gmail.com <julieannevl@gmail.com>
2018-03-05 18:02:01 +0100

    created controller, checking on method functinality

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,23 +1,46 @@
 class DestinationsController < ApplicationController
+  before_action :set_destination, only: [:show, :edit, :update, :destroy]
+
   def index
-    @destinations = Destination.New
+    @destinations = Destination.all
   end
 
   def show
-    @destination.find(params[:id])
-    redirect_
+
   end
 
   def new
+    @destinations = Destination.new
   end
 
   def create
+    destination.create(params_destination)
+    destination.save
+    redirect_to destination
   end
 
   def edit
+
   end
 
   def update
+    @destination.update(destination_params)
+    redirect to destination_path(destination)
   end
 
+  def destroy
+    redirect to
+  end
+
+private
+
+def set_destination
+  @destination = Destination.find(params[:id])
 end
+
+  def destination_params
+    params.require(:destination).permit(:name, :description, :address, :latitude, :longitude, :type)
+  end
+end
+
+
