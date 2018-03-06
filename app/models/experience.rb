44e79db5774b937ed8bb7db0a3243cfcbd22d5f0
Matt Roach <mattroach@mac.com>
2018-03-06 17:46:07 +0100

    geocode fix

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -6,7 +6,7 @@ class Experience < ApplicationRecord
 
 # TO GET GEOCODE WHERE ADDRESS THAT IT BROKEN UP INTO SEPARATE COLUMNS IN THE TABLE
   geocoded_by :full_address
-  after_create :geocode, if: :will_save_change_to_address?
+  after_validation :geocode, if: :will_save_change_to_address?
 
   def full_address
     "#{street} #{number}, #{locality}, #{country}, #{region}"
