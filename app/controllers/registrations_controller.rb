b1aaf616dd93f7d9af223132c7275e8b9a4647dd
Owen Moss <owenkmoss@gmail.com>
2018-03-06 13:52:42 +0100

    trying to test admin role

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,10 @@
+class RegistrationsController < Devise::RegistrationsController
+  def new
+    raise
+    super
+  end
+
+  def create
+    raise
+  end
+end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,10 @@
+class SessionsController < Devise::SessionsController
+  private
+  def respond_to_on_destroy
+    # We actually need to hardcode this as Rails default responder doesn't
+    # support returning empty response on GET request
+    respond_to do |format|
+      format.all { head :no_content }
+      format.any(*navigational_formats) { redirect_to admin_experiences_path }
+    end
+  end
