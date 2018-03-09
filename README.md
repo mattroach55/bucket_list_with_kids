ea56806f61f63fa5512ceb3f1e416c0c58941382
Owen Moss <owenkmoss@gmail.com>
2018-03-09 12:10:58 +0100

    finished algolia searchbar

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 Rails app generated with [lewagon/rails-templates](https://github.com/lewagon/rails-templates), created by the [Le Wagon coding bootcamp](https://www.lewagon.com) team.
 
+Algolia
+
+To Reindex the models and update Algolia
+
+1. Rails.application.eager_load! # Ensure all models are loaded (required in development).
+
+2. algolia_models = ActiveRecord::Base.descendants.select{ |model| model.respond_to?(:reindex) }
+
+OR
+
 [Accommodation, Destination, Experience].map { |e| e.reindex! }
+
+3. algolia_models.each(&:reindex)
+
+If you target a single index from several models, you must never use MyModel.reindex and only use MyModel.reindex!. The reindex method uses a temporary index to perform an atomic reindexing: if you use it, the resulting index will only contain records for the current model because it will not reindex the others.
--- app/models/accommodation.rb
+++ app/models/accommodation.rb
@@ -12,17 +12,22 @@ class Accommodation < ApplicationRecord
     "#{street} #{street_number}, #{locality}, #{country}, #{region}"
   end
 
-  def algolia_id
-    "accomodation_#{id}" # ensure the teacher & student IDs are not conflicting
-  end
 
   include AlgoliaSearch
-  algoliasearch index_name: 'dev_BUCKETKIDS' do
+  algoliasearch index_name: 'dev_BUCKETKIDS', id: :algolia_id do
     attributes :name, :entity, :description, :street, :locality, :region,
-    :country, :holiday_type, :theme, :bucket_list_count, :average_review_score
+    :country, :holiday_type, :theme, :bucket_list_count, :average_review_score,
+    :kids_age_from, :photo, :kids_age_to, :connecting_rooms, :price, :pool, :beach,
+    :show, :duration
 
     searchableAttributes ['name', 'entity', 'unordered(description)']
 
     customRanking ['desc(bucket_list_count)']
+  end
+
+  private
+
+  def algolia_id
+    "#{id}" # ensure the teacher & student IDs are not conflicting
   end
 end
