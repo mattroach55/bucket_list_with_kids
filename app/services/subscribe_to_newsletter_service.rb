3214d1217d3d8d18e45d6fb4e0e112520bcc2278
Owen Moss <owenkmoss@gmail.com>
2018-03-14 12:44:00 +0100

    fixed the bug

--- app/services/subscribe_to_newsletter_service.rb
+++ app/services/subscribe_to_newsletter_service.rb
@@ -9,11 +9,10 @@ class SubscribeToNewsletterService
     @gibbon.lists(@list_id).members.create(
       body: {
         email_address: @user.email,
-        status: "subscribed",
-        merge_fields: {
-          FNAME: @user.first_name,
-          LNAME: @user.last_name
-        }
+        status: "subscribed"
+        # merge_fields: {
+        #   FNAME: @user.username
+        # }
       }
     )
   end
