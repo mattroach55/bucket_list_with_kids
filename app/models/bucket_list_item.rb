78e285b4581e7dd9b0e6aede4b76a25ec1868d10
Juanito Alvaro <juanitoalvaro@live.nl>
2018-03-15 17:55:00 +0100

    adding right paths from bucketlist

--- app/models/bucket_list_item.rb
+++ app/models/bucket_list_item.rb
@@ -3,4 +3,14 @@ class BucketListItem < ApplicationRecord
   belongs_to :experience, optional: true
   belongs_to :destination
   belongs_to :user
+
+  # def entity
+  #   if accommodation_id != nil
+  #     return "accommodation"
+  #   elsif experience_id != nil
+  #     return "experience"
+  #   elsif destination_id != nil
+  #     return "destination"
+  #  end
+  #end
 end
